af9e11f062d266dc56944fa50cdb1cb5e5f89938
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-03-20 22:10:30 +1000

    added host option to database setup

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 # Connect to your postgres DB
 conn = psycopg2.connect(
+    host=os.getenv('DB_HOST'),
     dbname=os.getenv('DB_NAME'),
     user=os.getenv('DB_USER'),
     password=os.getenv('DB_PASSWORD')
