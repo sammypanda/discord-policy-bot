8b76e7e2c673a2502777615a78f421524b623d6b
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-08-22 17:23:40 +1000

    [optimisation] _console_manager -> _console
    the variable for managing the bot from the server/console was unecessarily long

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 from models.manager_model import ManagerModel
 from controllers.manager_controller import ManagerController
 
-_console_manager = ManagerModel() # instantiate a private manager_model just for console/server admin
-_bot = _console_manager.get_bot() # private variable for the ongoing bot instance
+_console = ManagerModel() # instantiate a private manager_model just for console/server admin
+_bot = _console.get_bot() # private variable for "managing" the ongoing bot instance
 
 # Setup 
 manager = ManagerController(_bot) # instantiate the manager commands
-_console_manager.run_bot(_bot) # instantiate the bot
+_console.run_bot(_bot) # instantiate the bot
