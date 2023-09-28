ab3dd87118749731ee2553e710ac5dad9f686e87
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-09-28 23:51:27 +1000

    adding dedicated logging mechanism (testing)

--- models/generator_model.py
+++ models/generator_model.py
@@ -1,10 +1,15 @@
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv() # import dotenv values
 
 class GeneratorModel:
+    def __init__(self):
+        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
     def log(self, level: int, message: str):
         if int(os.getenv('DEBUG_LEVEL')) >= int(level):
             print(message)
+            logging.info(message) # before committing, investigate if this actually is worthwhile
         
